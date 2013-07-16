5bb68be4f7a8f9e445b4ef569d4a26e00fcc3ea6
episodeyang <yangge1987@gmail.com>
2013-07-16 07:16:41 -0500

    Adding Report page WIP

--- client/js/controllers/reports.js
+++ client/js/controllers/reports.js
@@ -12,6 +12,20 @@ angular.module('SpruceQuizApp')
             $scope.rankFakeData = ['a data point'];
             $scope.orderProp = 'lastupdated';
             $scope.expression = "a math expression: \\(\\sqrt{\\frac{1}{2}}\\)";
+            $scope.class={
+                "id1":{
+                    "students":{
+                        "studentId1":{
+                            "examScores":{
+                                "数学第一次练习"：100,//key 的名称就是考试的名字
+                                "数学第二次练习"：100,
+                            },
+                        "someList":[1,2,3,4,5]
+                        },
+                    "distribution":{}
 
+                    }
+                },
+            }
         }]);
 
