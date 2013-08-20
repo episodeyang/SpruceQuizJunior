7f99da474541d937fdfb8809ff9b2930a5852bf7
yifanwu <yifan1030@gmail.com>
2013-08-20 09:29:31 -0700

    added pie chart but not changed style of code yet

--- client/js/directives/d3/reports_viz.js
+++ client/js/directives/d3/reports_viz.js
@@ -1,57 +1,79 @@
 'use strict';
 
-var createSVG, updateBarChart, updateLineGraph;
+var createSVG, updateBarChart, updateLineGraph, updatePieChart;
 
 angular.module('SpruceQuizApp')
-    .directive('studentExamViz', function () {
+    .directive('d3BarChart', function () {
 
-      return {
-        restrict: 'CE',
-        terminal: true,
-        scope: {
-          data: '='
-        },
-        link: function (scope, element, attrs) {
-          // set the values up
-          scope.h = 300;
-          scope.barW = 40;
-          scope.w = (scope.barW + 30) * scope.data.length;
-          console.log(scope.w);
-          console.log(scope.data.length);
+    return {
+      restrict: 'CE',
+      terminal: true,
+      scope: {
+        data: '='
+      },
+      link: function (scope, element, attrs) {
+        // set the values up
+        scope.h = 300;
+        scope.barW = 40;
+        scope.w = (scope.barW + 30) * scope.data.length;
+        console.log(scope.w);
+        console.log(scope.data.length);
 
-          // call function to make graph
-          createSVG(scope, element);
-          scope.$watch('data', updateBarChart, true);
+        // call function to make graph
+        createSVG(scope, element);
+        scope.$watch('data', updateBarChart, true);
 
-        }
-      };
+      }
+    };
 
     });
 
 angular.module('SpruceQuizApp')
-    .directive('studentSubjectViz', function () {
+    .directive('d3LineGraph', function () {
 
-      return {
-        restrict: 'CE',
-        terminal: true,
-        scope: {
-          data: '='
-        },
-        link: function (scope, element, attrs) {
-          // set the values up
-          scope.h = 300;
-          scope.margin = 20;
-          scope.w = 400;
-          console.log(scope.w);
-          console.log(scope.data.length);
+    return {
+      restrict: 'CE',
+      terminal: true,
+      scope: {
+        data: '='
+      },
+      link: function (scope, element, attrs) {
+        // set the values up
+        scope.h = 300;
+        scope.margin = 20;
+        scope.w = 400;
+        console.log(scope.w);
+        console.log(scope.data.length);
 
-          // call function to make graph
-          createSVG(scope, element);
-          scope.$watch('data', updateLineGraph, true);
+        // call function to make graph
+        createSVG(scope, element);
+        scope.$watch('data', updateLineGraph, true);
 
-        }
-      };
-    });
+      }
+    };
+  });
+
+angular.module('SpruceQuizApp')
+    .directive('d3PieChart', function () {
+    return {
+      restrict: 'CE',
+      terminal: true,
+      scope: {
+        data: '='
+      },
+      link: function (scope, element, attrs) {
+        // set the values up
+        scope.h = 400;
+        scope.r = 150;
+        scope.w = 400;
+
+        // call function to make graph
+        createSVG(scope, element);
+        scope.$watch('data', updatePieChart, true);
+
+      }
+    };
+  });
 
 // don't create if already exist
 // avoid creating more than more svg
@@ -64,6 +86,9 @@ createSVG = function (scope, element) {
         .attr("height", scope.h);
   }
 };
+
+
+
 
 // helper functions
 updateBarChart = function (newVal, oldVal, scope) {
@@ -174,3 +199,46 @@ updateLineGraph = function (newVal, oldVal, scope) {
       .attr("x2", x(0));
 
 };
+
+updatePieChart = function (newVal, oldVal, scope) {
+
+  console.log(newVal);
+  var arc, pie, arcs, color;
+  // more minor helpers
+  color = d3.scale.category20c();
+
+  scope.svg
+      .data([newVal])                   //associate our data with the document
+      .attr("width", scope.w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+      .attr("height", scope.h)
+      .append("svg:g")               //make a group to hold our pie chart
+      .attr("transform", "translate(" + 1.1 * scope.r + "," + scope.r + ")");    //move the center of the pie chart from 0, 0 to radius, radius
+
+  arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+      .outerRadius(scope.r);
+
+  pie = d3.layout.pie()           //this will create arc data for us given a list of values
+      .value(function (d) { return d.value; });    //we must tell it out to access the value of each element in our data array
+
+  arcs = scope.svg.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+      .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
+      .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+      .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+      .attr("class", "slice")    //allow us to style things in the slices (like text)
+      .attr("transform", "translate(" + 1.1 * scope.r + "," + scope.r + ")");
+
+  arcs.append("svg:path")
+      .attr("fill", function (d, i) { return color(i); }) //set the color for each slice to be chosen from the color function defined above
+      .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+
+  arcs.append("svg:text")                                     //add a label to each slice
+      .attr("transform", function (d) {                    //set the label's origin to the center of the arc
+        //we have to make sure to set these before calling arc.centroid
+      d.innerRadius = 0;
+      d.outerRadius = scope.r;
+      return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+    })
+    .attr("text-anchor", "middle")                          //center the text on it's origin
+    .text(function (d, i) { return scope.data[i].label; });        //get the label from our original data array
+
+}
