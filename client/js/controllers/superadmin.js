8a76de5371a7b6272a7c1ce0feb8964f0ebda1de
yifanwu <yifan1030@gmail.com>
2013-09-10 16:57:35 -0700

    directive almost functioning

--- client/js/controllers/superadmin.js
+++ client/js/controllers/superadmin.js
@@ -1,81 +1,106 @@
 'use strict';
-angular.module('SpruceQuizApp')
-    .controller('SuperAdminCtrl',
-        ['$rootScope', '$scope', 'Sections', 'Units', 'Materials', 'Students',
-            function ($rootScope, $scope, Sections, Units, Materials, Students) {
-                // this object is to hold data for future tables
-                $scope.values = {};
-                $scope.studentShowClicked = false;
-                var itemsPerPage = 10;
-                $scope.currentPage = 0;
 
-                // helper function: calculate page
-                // takes in the list and returns a processed list of lists.
-                var groupToPages = function (items) {
-                    var pagedItems = [];
+var spApp = angular.module('SpruceQuizApp');
 
-                    console.log(items.length);
-                    for (var i = 0; i < items.length; i++) {
+spApp.controller('SuperAdminCtrl',
+    ['$scope', 'Students',//'$rootScope', '$scope', 'Sections', 'Units', 'Materials', 'Students',
+        function ($scope, Students) {//$rootScope, $scope, Sections, Units, Materials, Students) {
+            // this object is to hold data for future tables
+            $scope.data = {}; // initialized here and updated later when things are clicked.
+            $scope.studentShowClicked = false;
+            var itemsPerPage = 10;
+            $scope.currentPage = 0;
 
-                        if (i % itemsPerPage === 0) {
-                            pagedItems[Math.floor(i / itemsPerPage)] = [ items[i] ];
-                        } else {
-                            pagedItems[Math.floor(i / itemsPerPage)].push(items[i]);
-                        }
+            // helper function: calculate page
+            // takes in the list and returns a processed list of lists.
+            var groupToPages = function (items) {
+                var pagedItems = [];
+
+                console.log(items.length);
+                for (var i = 0; i < items.length; i++) {
+
+                    if (i % itemsPerPage === 0) {
+                        pagedItems[Math.floor(i / itemsPerPage)] = [ items[i] ];
+                    } else {
+                        pagedItems[Math.floor(i / itemsPerPage)].push(items[i]);
                     }
+                }
 
-                    console.log(pagedItems);
-                    return pagedItems;
-                };
+                console.log(pagedItems);
+                return pagedItems;
+            };
 
-                $scope.range = function (start, end) {
-                    var ret = [];
-                    if (!end) {
-                        end = start;
-                        start = 0;
-                    }
-                    for (var i = start; i < end; i++) {
-                        ret.push(i);
-                    }
-                    return ret;
-                };
+            $scope.range = function (start, end) {
+                var ret = [];
+                if (!end) {
+                    end = start;
+                    start = 0;
+                }
+                for (var i = start; i < end; i++) {
+                    ret.push(i);
+                }
+                return ret;
+            };
 
-                $scope.prevPage = function () {
-                    if ($scope.currentPage > 0) {
-                        $scope.currentPage--;
-                    }
-                };
+            $scope.prevPage = function () {
+                if ($scope.currentPage > 0) {
+                    $scope.currentPage--;
+                }
+            };
 
-                $scope.nextPage = function () {
-                    if ($scope.currentPage < $scope.pagedItems.length - 1) {
-                        $scope.currentPage++;
-                    }
-                };
+            $scope.nextPage = function () {
+                if ($scope.currentPage < $scope.pagedItems.length - 1) {
+                    $scope.currentPage++;
+                }
+            };
 
-                $scope.setPage = function () {
-                    $scope.currentPage = this.n;
-                };
+            $scope.setPage = function () {
+                $scope.currentPage = this.n;
+            };
 
-                $scope.model = {};
-                $scope.listStudents = function () {
-                    $scope.studentShowClicked = true;
-                    $scope.values['student'] = {};
-                    Students.onStudents.list(function(results){
-                        // the controller gets the data and puts in the correct data structure
-                        $scope.values['student'].header = [
-                            {value:'name',name:'Name'},
-                            {value:'dateOfBirth',name:'DoB'},
-                            {value:'gender',name:'Gender'},
-                            {value:'schools',name:'Schools'},
-                            {value:'sections',name:'Sections'},
-                            {value:'phone',name:'Phone'}
-                        ];
-                        $scope.values['student'].data = groupToPages(results);
-                        //$scope.model.studentList =
-                        console.log("direct results are:");
-                        console.log(results);
-                    });
-                };
-            }
-        ]);
+//                $scope.testVal = [
+//                    {value: 'name', name: 'Name'},
+//                    {value: 'dateOfBirth', name: 'DoB'},
+//                    {value: 'gender', name: 'Gender'},
+//                    {value: 'schools', name: 'Schools'},
+//                    {value: 'sections', name: 'Sections'},
+//                    {value: 'phone', name: 'Phone'}
+//                ];
 
+            $scope.listStudents = function () {
+                $scope.studentShowClicked = true;
+                $scope.data['student'] = {};
+                Students.onStudents.list(function (results) {
+                    // the controller gets the data and puts in the correct data structure
+                    $scope.testVal = [
+                        {value: 'name', name: 'Name'},
+                        {value: 'dateOfBirth', name: 'DoB'},
+                        {value: 'gender', name: 'Gender'},
+                        {value: 'schools', name: 'Schools'},
+                        {value: 'sections', name: 'Sections'},
+                        {value: 'phone', name: 'Phone'}
+                    ];
+                    $scope.data['student'].header = [
+                        {value: 'name', name: 'Name'},
+                        {value: 'dateOfBirth', name: 'DoB'},
+                        {value: 'gender', name: 'Gender'},
+                        {value: 'schools', name: 'Schools'},
+                        {value: 'sections', name: 'Sections'},
+                        {value: 'phone', name: 'Phone'}
+                    ];
+                    $scope.data['student'].data = groupToPages(results);
+                    //$scope.model.studentList =
+                    console.log("direct results are:");
+                    console.log(results);
+                });
+            };
+        }
+    ]
+);
+
+//spApp.controller('SuperAdminCtrl',
+//    ['$scope', 'Students',//'$rootScope', '$scope', 'Sections', 'Units', 'Materials', 'Students',
+//        function ($scope, Students) {
+//        }
+//    ]
+//);
--- client/js/directives/superadmin_dir.js
+++ client/js/directives/superadmin_dir.js
@@ -2,63 +2,72 @@
 
 var spApp = angular.module('SpruceQuizApp');
 
+// declarations
+var watchTest;
+
+spApp.directive('dirTest', function () {
+    var myHTML = "<h1>{{val}}</h1>";
+    return {
+        restrict: 'E',
+        template: myHTML, // '<div>{{val}}</div>',
+        link: function (scope) {
+            scope.val = "hello world";
+        }
+    };
+});
+
 spApp.directive('jsonTable', function () {
+
+    // putting the variable here so it doesn't clogger up
+    var tableTemplate =
+        '<div class="row-fluid">' +
+            '<div class="span2">Sort by:</div>' +
+            '<div class="span4"><select ng-model="orderProp" ng-options="key.name for key in data[\'student\'].header"></select></div>' + //{{data[type].header}}
+            '<div class="span1">Query:</div>' +
+            '<div class="span4"><input ng-model="query"></div>' +
+            '<table class="table table-striped table-condensed table-hover">' +
+            '<thead><tr>' +
+            '<th ng-repeat="key in data[\'student\'].header">{{key.name}}</th>' +
+            '</tr></thead>' +
+            '<tfoot><tr>' +
+            '<td colspan="6">' +
+            '<div class="pagination pull-right">' +
+            '<ul>' +
+            '<li ng-class="{disabled: currentPage == 0}"><a href="href" ng-click="prevPage()">«' +
+            'Prev</a></li>' +
+            '<li ng-repeat="n in range(pagedItems.length)" ng-class="{active: n == currentPage}"' +
+            'ng-click="setPage()"><a href="href" ng-bind="n + 1">1</a></li>' +
+            '<li ng-class="{disabled: currentPage == 0 || currentPage &lt;= pagedItems.length - 1}">' +
+            '<a href="href" ng-click="nextPage()">Next »</a></li>' +
+            '</ul></div></td></tr></tfoot>' +
+            '<tbody>' +
+            '<tr ng-repeat="item in data[type].data [currentPage] | filter:query | orderBy:orderProp">' +
+        '</table></div>';
+
+    // no need to have isolate scope because this directive need data from the controller's scope
+    // FIXME: this is bad practice since it might pollute the scope but I'm not sure how to fix
     return {
         restrict: 'CE',
-        terminal: true,
+        replace: true,        // replace original markup with template
+        template: tableTemplate,
         scope: {
-            type: '=',
-            data: '='
+            type: "@",
+            data: "="
         },
-//        replace: true,        // replace original markup with template
-//        transclude: false,    // do not copy original HTML content
         link: function (scope, element, attrs) {
-            // a mapping from key word to data and headers (in the form of a tuple)
-            //$scope.listVarName = 'model.studentList'
-            // type is the iterator that's passed in to
-            console.log("the directive is at least called");
-            var th_list = '';
-            var td_list = '';
-            for (var key in data[type].header) {
-                th_list.append('<th class="' + key.value + '">' + key.name + '</th>\n');
-                td_list.append('<td>{{' + type + '.' + key.value + '.toString()}}<td>\n');
-            }
+            console.log("from directive");
+            console.log(scope.data['student'].header);
 
-
-            // the data should have already been fetched from the controller
-            //colDataValName is a local variable passed to the function
-            // which need to be stored
-            //$scope.colDataValName = 'model.studentListColData'
-            var htmlText =
-                '<div class="row-fluid">' +
-                    '<div class="span2">Sort by:</div>' +
-                    '<div class="span4">' +
-                    '<select ng-model="orderProp" ng-options="key.name for key in' + colDataValName + '></select></div>' +
-                    '<div class="span1">Query:</div>' +
-                    '<div class="span4"><input ng-model="query"></div>' +
-                    '<table class="table table-striped table-condensed table-hover">' +
-                    '<thead><tr>'
-                    + th_list +
-                    '</tr></thead>' +
-                    '<tfoot><tr>' +
-                    '<td colspan="6">' +
-                    '<div class="pagination pull-right">' +
-                    '<ul>' +
-                    '<li ng-class="{disabled: currentPage == 0}"><a href="href" ng-click="prevPage()">«' +
-                    'Prev</a></li>' +
-                    '<li ng-repeat="n in range(pagedItems.length)" ng-class="{active: n == currentPage}"' +
-                    'ng-click="setPage()"><a href="href" ng-bind="n + 1">1</a></li>' +
-                    '<li ng-class="{disabled: currentPage == 0 || currentPage &lt;= pagedItems.length - 1}">' +
-                    '<a href="href" ng-click="nextPage()">Next »</a></li>' +
-                    '</ul></div></td></tr></tfoot>' +
-                    '<tbody>' +
-                    '<tr ng-repeat="item in ' + data[type].data + '[currentPage] | filter:query | orderBy:orderProp">' +
-                    'td_list' +
-                    '</tr></tbody></table>';
-            //.html
-            //$scope.watch()
-            element.replaceWith(htmlText);
+            //scope.$watch('testVal', watchTest, true);
         }
-
-    }
+    };
 });
+
+// directive listeners (from $watch)
+watchTest = function (newVal, oldVal, scope) {
+    scope.testVal = oldVal;
+    console.log("logging from directive");
+    console.log(newVal);
+    console.log(oldVal);
+}
+
