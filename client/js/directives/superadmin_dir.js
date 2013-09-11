cfb4f2dfb34acf1dd5baeeccde367142e70640bc
yifanwu <yifan1030@gmail.com>
2013-09-11 14:17:26 -0700

    sort of completed directive (still have data binding issues)
    currently hard coded currentPage, and haven't done other tabs

--- client/js/directives/superadmin_dir.js
+++ client/js/directives/superadmin_dir.js
@@ -6,13 +6,16 @@ var spApp = angular.module('SpruceQuizApp');
 var watchTest;
 
 spApp.directive('dirTest', function () {
-    var myHTML = "<h1>{{val}}</h1>";
+    var myHTML = "<h1>{{pageStatus.currentPage}}</h1>";
     return {
         restrict: 'E',
-        //template: myHTML, // ,
+        template: myHTML,
+//        scope: {
+//            pageStatus: "="
+//        },
         link: function (scope, element) {
             scope.val = "hello world";
-            element.replaceWith('<div>Object.toString()</div>');
+//            element.replaceWith('<div>Object.toString()</div>');
         }
     };
 });
@@ -22,8 +25,11 @@ spApp.directive('jsonTable', function () {
     // putting the variable here so it doesn't clogger up
     var tableTemplate =
         '<div class="row-fluid">' +
+            //'<p>{{data[\'student\'].data[0]}}</p>'+
+            //'<p>{{pageStatus}}</p>'+
+            //'<p>{{pageStatus.currentPage}}</p>'+
             '<div class="span2">Sort by:</div>' +
-            '<div class="span4"><select ng-model="orderProp" ng-options="key.name for key in data[\'student\'].header"></select></div>' + //{{data[type].header}}
+            '<div class="span4"><select ng-model="orderProp" ng-options="key.name for key in data[type].header"></select></div>' + //{{data[type].header}}
             '<div class="span1">Query:</div>' +
             '<div class="span4"><input ng-model="query"></div>' +
             '<table class="table table-striped table-condensed table-hover">' +
@@ -31,8 +37,8 @@ spApp.directive('jsonTable', function () {
             '<th ng-repeat="key in data[\'student\'].header">{{key.name}}</th>' +
             '</tr></thead>' +
             '<tbody>' +
-            '<tr ng-repeat="item in data[\'student\'].data [currentPage] | filter:query | orderBy:orderProp">' +
-            '<td ng-repeat="key in data[\'student\'].header">{{student[key.value].toString()}}</td>' +
+            '<tr ng-repeat="item in data[\'student\'].data[0] | filter:query | orderBy:orderProp">' +
+            '<td ng-repeat="key in data[\'student\'].header">{{item[key.value].toString()}}</td>' +
             '</tr></tbody>' +
             '<tfoot><tr>' +
             '<td colspan="6">' +
@@ -54,19 +60,33 @@ spApp.directive('jsonTable', function () {
         template: tableTemplate,
         scope: {
             type: "@",
-            data: "="
+            data: "=",
+            pageStatus: "="
         },
         link: function (scope, element, attrs) {
-            console.log(scope.type);
-            scope.$watch('data', function (newVal, oldVal) {
-                console.log("logging from watch of directive, and the new value is:");
-                console.log("changing, the new val is:");
-                console.log(newVal);
-                console.log(oldVal);
-                //element.replaceWith(tableTemplate);
-            });
+            console.log("current page");
+            console.log(scope.pageStatus.currentPage);
+            console.log(scope.studentShowChecked);
+
         }
-    };
+//            console.log(scope.type);
+//            console.log(scope.data['student'].header);
+//            console.log(scope.data['student'].data);
+//            scope.$watch('data', function (newVal, oldVal) {
+//                //console.log("logging from watch of directive, and the new value is:");
+////                console.log("changing, the new val is:");
+//                //console.log(newVal);
+//                //console.log(oldVal);
+//                //element.replaceWith(tableTemplate);
+//                console.log('show new value')
+//                console.log(newVal);
+//                console.log('show old value');
+//                console.log(oldVal);
+//                console.log(scope.data['student'].header);
+//                console.log(scope.data['student'].data);
+//            });
+        };
+    //};
 });
 
 
