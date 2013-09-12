93d13212ecf851ad4712848e10fac383f1bddc61
yifanwu <yifan1030@gmail.com>
2013-09-11 17:49:39 -0700

    added some more detailed comments for the test

--- client/js/modelServiceSpec.js
+++ client/js/modelServiceSpec.js
@@ -1,15 +1,12 @@
 /**
  *
- * Created with JetBrains WebStorm.
- * User: Ge Yang
- * Date: 9/10/13
- * Time: 8:41 PM
- * To change this template use File | Settings | File Templates.
+ * front end spec code using Karma and Jasmin
  */
 
 'use strict';
 
 describe('Model test', function () {
+    // a rather trivial hello world example, need to clean up later
     describe('when I call Model.test', function () {
         it('returns 1', function () {
             var $injector = angular.injector([ 'modelServices','ngResource' ]);
