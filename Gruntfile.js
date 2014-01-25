7fdb02d3e0a2605cae2157f11c6f5271b4839672
episodeyang <yangge1987@gmail.com>
2014-01-25 13:45:51 -0600

    started server-side unit testing setup. Need testing script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,23 +9,33 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
         mochaTest: {
-            all: {
+            test: {
                 options: {
-                    reporter: 'spec'
+//                    reporter: 'spec'
+                    reporter: 'Spec',
+                    // Require blanket wrapper here to instrument other required
+                    // files on the fly.
+                    //
+                    // NB. We cannot require blanket directly as it
+                    // detects that we are not running mocha cli and loads differently.
+                    //
+                    // NNB. As mocha is 'clever' enough to only run the tests once for
+                    // each file the following coverage task does not actually run any
+                    // tests which is why the coverage instrumentation has to be done here
+                    require: 'coverage/blanket' //Need to fix later
                 },
                 src: ['server/tests/**/*.js']
             },
-            requirejs: {
-                src: 'server/tests/**/*.js',
+            coverage: {
                 options: {
-                    reporter: 'spec',
-                    rjsConfig: {
-                        baseUrl: "./",
-                        paths: {
-                            foo: "bar/v1.0"
-                        }
-                    }
-                }
+                    reporter: 'html-cov',
+                    // use the quiet flag to suppress the mocha console output
+                    quiet: false,
+                    // specify a destination file to capture the mocha
+                    // output (the quiet option does not suppress this)
+                    captureFile: 'coverage.html'
+                },
+                src: ['server/tests/**/*.js']
             }
         },
         watch: {
@@ -92,7 +102,8 @@ module.exports = function (grunt) {
 //        'lint',
         'test',
         'jsdoc']);
-    grunt.registerTask('test', 'mochaTest:all');
+    grunt.registerTask('test', 'mochaTest:test');
+    grunt.registerTask('coverage', 'mochaTest:coverage');
     grunt.registerTask('watch-test', 'watch:mochaTest');
     grunt.registerTask('watch-doc', 'watch:jsdoc');
 
