56e547c322eaa4c127fc5d088f397e094f05d3e3
Ge Yang <yangge1987@gmail.com>
2014-03-11 18:50:35 -0500

    very good, modal form controller is working

--- client/js/controllers/login.js
+++ client/js/controllers/login.js
@@ -1,13 +1,23 @@
 'use strict';
 /* Controllers */
 angular.module('SpruceQuizApp')
-    .controller('NavbarCtrl',
+    .controller('LoginCtrl',
         ['$routeParams', '$location', '$filter', '$rootScope', '$scope', '$modal', 'Auth', 'Sections', 'Units', 'Materials', 'Students', 'Model',
             function ($routeParams, $location, $filter, $rootScope, $scope, $modal, Auth, Sections, Units, Materials, Students, Model) {
                 //Boiler Plate for authentication info
                 $scope.user = Auth.user;
                 $scope.userRoles = Auth.userRoles;
                 $scope.accessLevels = Auth.accessLevels; //to allow authentication based elements.
+
+
+                $scope.loginData = {
+                    username: 'hahaha',
+                    password: 'default password'
+                }
+
+                $scope.submit = function () {
+                    alert($scope.loginData.username);
+                }
             }
         ]
     );
