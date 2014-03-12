58365e4d51c587344067f159206f498d00371fd4
Ge Yang <yangge1987@gmail.com>
2014-03-11 23:22:19 -0500

    initial update on registration form

--- client/js/controllers/register.js
+++ client/js/controllers/register.js
@@ -2,8 +2,8 @@
 /* Controllers */
 angular.module('SpruceQuizApp')
     .controller('RegisterCtrl',
-        ['$routeParams', '$location', '$filter', '$rootScope', '$scope', '$modal', 'Auth', 'Sections', 'Units', 'Materials', 'Students', 'Model',
-            function ($routeParams, $location, $filter, $rootScope, $scope, $modal, Auth, Sections, Units, Materials, Students, Model) {
+        ['$routeParams', '$location', '$filter', '$rootScope', '$scope', '$timeout', '$modal', 'Auth', 'Sections', 'Units', 'Materials', 'Students', 'Model',
+            function ($routeParams, $location, $filter, $rootScope, $scope, $timeout, $modal, Auth, Sections, Units, Materials, Students, Model) {
                 //Boiler Plate for authentication info
                 $scope.user = Auth.user;
                 $scope.userRoles = Auth.userRoles;
@@ -20,23 +20,97 @@ angular.module('SpruceQuizApp')
                     value : Auth.userRoles.parent
                     }
                 ]
+
                 $scope.registerData = {
-                }
+                    username: '',
+                    email: '',
+                    schoolName: '',
+                    password: '',
+                    role: {},
+                    firstName: '',
+                    lastName: '',
+                    dateOfBirth: '',
+                    gender: ''
+                };
+                $scope.exitRegister = function(){
+                    $scope.viewCtrl.showRegistForm = false;
+                };
 
-                $scope.submit = function () {
+                $scope.view.clearErrors =
+                    function () {
+                        $scope.view.error = undefined;
+                    }
+
+                var dateFormater = function(rawString){
+                    "use strict";
+                    var len = 0;
+                    if (rawString){
+                        len = rawString.length;
+//                        console.log(len);
+//                        console.log('show the length')
+                    } else {rawString = ''}
+                    if (len == 5 || len == 8){
+                        if (rawString.substring(len-1,len)!='-'){
+                            return rawString.substring(0, len-1)+"-"+rawString.substring(len-1, len);
+//                            console.log('fit the length')
+                        } else {return rawString.substring(0, len-1);}
+                    }
+                    else if (len>=11){ return rawString.substring(0,10);}
+                    else {return rawString;}
+                };
+
+                $scope.$watch('registerData.birthdayString', function(newValue, oldValue){
+                    $scope.registerData.birthdayString = dateFormater(newValue);
+                    $scope.registerData.dateOfBirth = new Date($scope.registerData.birthdayString.split('_'))
+                })
+
+                $scope.submit = function() {
                     var that = this;
-                    Auth.register(
-                        $scope.loginData,
-                        function(res) {
-                            Model.init();
-                            Model.queryQuestions();
-                            that.$hide();
-                        },
-                        function(err) {
-                            $rootScope.errors.login = "Failed to login";
-                        });
+                    if ($scope.registerData.password !== $scope.registerData.passwordConfirm){
+                        $scope.view.error = '两次密码输入不相符。请再次输入密码。';
+                        console.log($scope.view.error);
+                    }
+                    if ($scope.registerData.role == null ){
+                        $scope.view.error = '请定义用户身份。';
+                        console.log($scope.view.error);
+                    } else {
+                        $scope.registerData.role = Auth.userRoles[$scope.registerData.role];
+                    };
+                    if (!$rootScope.err){
+                        console.log('sending registration data to /regist')
+                        var query = {
+                                username: $scope.registerData.username,
+                                password: $scope.registerData.password,
+                                role: $scope.registerData.role,
+                                params: {
+                                    email: $scope.registerData.email,
+                                    schoolName: $scope.registerData.schoolName,
+                                    name: $scope.registerData.lastName+','+$scope.registerData.firstName,
+                                    DOB: $scope.registerData.dateOfBirth
+                                }
+                            };
+                        console.log(query)
+                        Auth.register(
+                            query,
+                            function(res) {
+//                                console.log('now move to path "/"')
+                                $location.path('/questions');
+                                Model.init();
+                                Model.queryQuestions();
+                                that.$hide();
+                            },
+                            function(err) {
+                                if ($scope.view.error === undefined ) {
+                                    $scope.view.error = "注册信息有问题，请重新输入。";
+                                }
+                                $timeout(
+                                    $scope.view.clearErrors
+                                    , 3000);
 
-                }
+                            }
+                        );
+                    }
+                };
             }
         ]
     );
