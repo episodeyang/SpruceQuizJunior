1f45e8c2155ee52f9abe6d35f7ed1bd509b844cf
episodeyang <yangge1987@gmail.com>
2014-04-06 17:48:47 -0500

    add jsdoc string and process.env notifications to mailer.logins

--- server/mailer/logins.js
+++ server/mailer/logins.js
@@ -1,14 +1,23 @@
 /**
  * Created by ge on 3/29/14.
+ * @name logins
+ * @memberOf Mailer
+ * @param {string} NJ_MAILER_USER email address for mailer
+ * @param {string} NJ_MAILER_PASSWORD for mailer
+ * @return {object} logins for mailer
  */
-define(['../models/SchemaModels', 'nodemailer', './logins'],
-    function (SchemaModels, nodemailer, login) {
+define(['nodemailer'],
+    function (nodemailer) {
         if (typeof process.env.NJ_MAILER_PASSWORD != 'string') {
             console.log('NJ_MAILER_PASSWORD environmental variable not set. Need for nodemailer.');
             throw new Error("Cannot find variable NJ_MAILER_PASSWORD");
         }
+        if (typeof process.env.NJ_MAILER_USER != 'string') {
+            console.log('NJ_MAILER_USER environmental variable not set. Need for nodemailer.');
+            throw new Error("Cannot find variable NJ_MAILER_PASSWORD");
+        }
 
-        var login = {
+        var logins = {
             host: "smtp-mail.outlook.com",
             secureConnection: false, // TLS requires secureConnection to be false
             port: 587,
@@ -21,6 +30,6 @@ define(['../models/SchemaModels', 'nodemailer', './logins'],
             }
         };
 
-        return login;
+        return logins;
     }
 );
