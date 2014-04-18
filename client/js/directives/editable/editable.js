4ac7926361b81548f31f5306ca86a82de08a6673
Ge Yang <yangge1987@gmail.com>
2014-04-18 00:13:33 -0500

    add placeholder to contenteditable

--- client/js/directives/editable/editable.js
+++ client/js/directives/editable/editable.js
@@ -1,33 +1,43 @@
 /**
  * Created by ge on 4/17/14.
+ * adding escape and enter key trigger to the built-in
+ * contenteditable attribute.
  */
 var spApp = angular.module('SpruceQuizApp');
-spApp.directive('contentEditable', function() {
+spApp.directive('contenteditable', function () {
     return {
         require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl) {
+        link: function (scope, elm, attrs, ctrl) {
             // view -> model
-            elm.bind('blur', function() {
-                scope.$apply(function() {
+            function onBlur () {
+                scope.$apply(function () {
                     ctrl.$setViewValue(elm.html());
                 });
-            });
+            }
+            elm.bind('blur', onBlur);
 
             // model -> view
-            ctrl.render = function(value) {
+            ctrl.render = function (value) {
                 elm.html(value);
             };
 
             // load init value from DOM
-//            ctrl.$setViewValue(elm.html());
             ctrl.$render();
-            elm.bind('keydown', function(event) {
-                console.log("keydown " + event.which);
+            elm.bind('keydown', function (event) {
+                // console.log("keydown " + event.which);
                 var esc = event.which == 27,
+                    enter = event.which == 13,
                     el = event.target;
 
                 if (esc) {
-                    console.log("esc");
+                    // console.log("esc");
+                    ctrl.$setViewValue(elm.html());
+                    el.blur();
+                    event.preventDefault();
+                }
+
+                if (enter) {
+                    // console.log("enter");
                     ctrl.$setViewValue(elm.html());
                     el.blur();
                     event.preventDefault();
