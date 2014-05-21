58383718323d8c86114666db76b6b3bb96a55e24
Ge Yang <yangge1987@gmail.com>
2014-05-20 22:08:52 -0500

    add bookEditCtrl skeleton code

---
+++ client/views/widgets/bookEdit.js
@@ -0,0 +1,24 @@
+'use strict';
+/* Controllers */
+angular.module('SpruceQuizApp')
+    .controller('BookEditCtrl',
+    ['$routeParams', '$location', '$filter', '$rootScope', '$scope', 'Auth', 'Sections', 'Units', 'Materials', 'Students', 'Model',
+        function ($routeParams, $location, $filter, $rootScope, $scope, Auth, Sections, Units, Materials, Students, Model) {
+            //Boiler Plate for authentication info
+            $scope.user = Auth.user;
+            $scope.userRoles = Auth.userRoles;
+            $scope.accessLevels = Auth.accessLevels; //to allow authentication based elements.
+
+            $scope.Model = Model;
+
+            $scope.view = {
+                state: 'list'
+            };
+            $rootScope.errors = {};
+
+            if ($routeParams.sessionId) {
+                Model.getSession($routeParams.sessionId);
+            }
+
+        }
+    ]);
